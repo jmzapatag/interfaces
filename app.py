f62946a95615f136933d00e3674f076a3debbb93
jmzapatag <69641302+jmzapatag@users.noreply.github.com>
2024-02-22 07:34:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,18 @@ st.write("no me copies anto")
 image = Image.open('ralsei.png')
 
 st.image(image, caption ='Ralsei my beloved')
+
+
+texto = st.text_input('hola', 'hola2')
+st.write('El texto escrito es', texto)
+
+st.subheader("ahora usemos dos columnas")
+
+col1, col2 = st.column(2)
+
+with col1:
+  st.subheader("Esta es la primera columna")
+  st.write("Las Interfaces multimodales mejoran la experiencia de usuario")
+  resp = st.checkbox("Estoy ed acuerdo")
+  if resp:
+    st.write('Correcto!')
